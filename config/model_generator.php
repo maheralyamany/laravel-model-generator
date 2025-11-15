1fc6cdebf2fa24514428b96ec1101add9477dcd4
maheralyamany <56364809+maheralyamany@users.noreply.github.com>
2025-11-15 17:02:50 +0300

    Change namespace to 'App\Models' and update db_types

--- config/model_generator.php
+++ config/model_generator.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-  'namespace' => 'App',
+  'namespace' => 'App\Models',
   'base_class_name' => \Illuminate\Database\Eloquent\Model::class,
   'output_path' => null,
   'no_timestamps' => null,
   'date_format' => null,
   'connection' => null,
   'no_backup' => null,
-  'db_types' => null,
+  'db_types' => [],
 ];
